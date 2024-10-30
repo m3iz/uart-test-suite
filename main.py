f6d1b6242ef7b3a01e65764cac898aac55c74363
Nick <nikita.saveli3v@yandex.ru>
2024-10-30 13:00:53 +0300

    Fix: command system has beed changed due to firmware fixes

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class SerialApp(QtWidgets.QWidget):
 
         self.initUI()
         self.serial_port = None
-        self.append_newline = False  # Flag to track newline character
 
     def initUI(self):
         self.setWindowTitle('UART Serial Communication')
@@ -25,7 +24,7 @@ class SerialApp(QtWidgets.QWidget):
 
         # ComboBox for baud rates
         self.baudrate_combobox = QtWidgets.QComboBox()
-        self.baudrate_combobox.addItems(["9600", "38400", "57600", "115200"])  # Предустановленные значения
+        self.baudrate_combobox.addItems(["1170", "1200", "57600", "115200"])  # Предустановленные значения
         layout.addWidget(self.baudrate_combobox)
 
         # Open/Close button
@@ -33,24 +32,31 @@ class SerialApp(QtWidgets.QWidget):
         self.open_button.clicked.connect(self.toggle_port)
         layout.addWidget(self.open_button)
 
-        # Checkbox for newline option (\r\n)
-        self.newline_checkbox = QtWidgets.QCheckBox('Send with \\r\\n (carriage return + newline)')
-        self.newline_checkbox.stateChanged.connect(self.toggle_newline)
-        layout.addWidget(self.newline_checkbox)
+        # Command buttons for GPIO control
+        self.command_buttons_layout = QtWidgets.QVBoxLayout()
 
-        # Command list for GPIO control
-        self.command_list = QtWidgets.QListWidget()
-        self.command_list.setVisible(False)  # Hidden initially, becomes visible on successful port open
-        layout.addWidget(self.command_list)
+        # Button for reading GPIO
+        self.read_gpio_button = QtWidgets.QPushButton('Read GPIO')
+        self.read_gpio_button.clicked.connect(self.send_read_gpio)
+        self.command_buttons_layout.addWidget(self.read_gpio_button)
 
-        # Input text field for additional commands
-        self.input_field = QtWidgets.QLineEdit()
-        layout.addWidget(self.input_field)
+        # Buttons for setting GPIO states
+        for i in range(2, 6):  # GP2 to GP5
+            set_high_button = QtWidgets.QPushButton(f'Set GP{i} High')
+            set_high_button.clicked.connect(lambda _, num=i: self.send_set_gpio_high(num))
+            self.command_buttons_layout.addWidget(set_high_button)
 
-        # Send button
-        self.send_button = QtWidgets.QPushButton('Send')
-        self.send_button.clicked.connect(self.send_data)
-        layout.addWidget(self.send_button)
+            set_low_button = QtWidgets.QPushButton(f'Set GP{i} Low')
+            set_low_button.clicked.connect(lambda _, num=i: self.send_set_gpio_low(num))
+            self.command_buttons_layout.addWidget(set_low_button)
+
+        # Initially hide the command buttons
+        for i in range(self.command_buttons_layout.count()):
+            button = self.command_buttons_layout.itemAt(i).widget()
+            if button:
+                button.setVisible(False)
+
+        layout.addLayout(self.command_buttons_layout)
 
         # Button to clear the output area
         self.clear_button = QtWidgets.QPushButton('Clear Output')
@@ -74,7 +80,11 @@ class SerialApp(QtWidgets.QWidget):
         if self.serial_port and self.serial_port.is_open:
             self.serial_port.close()
             self.open_button.setText('Open Port')
-            self.command_list.setVisible(False)
+            # Hide command buttons
+            for i in range(self.command_buttons_layout.count()):
+                button = self.command_buttons_layout.itemAt(i).widget()
+                if button:
+                    button.setVisible(False)
         else:
             selected_port = self.port_combobox.currentText()
             selected_baudrate = int(self.baudrate_combobox.currentText())  # Получаем выбранный baudrate
@@ -84,50 +94,23 @@ class SerialApp(QtWidgets.QWidget):
                 if self.serial_port.is_open:
                     self.output_message("Port opened successfully!", QtGui.QColor('green'))
                     self.open_button.setText('Close Port')
-                    self.populate_gpio_commands()
+                    # Show command buttons
+                    for i in range(self.command_buttons_layout.count()):
+                        button = self.command_buttons_layout.itemAt(i).widget()
+                        if button:
+                            button.setVisible(True)
             except serial.SerialException as e:
                 self.output_message(f"Error: {str(e)}", QtGui.QColor('red'))
 
-    def populate_gpio_commands(self):
-        """Populate the list with GPIO control commands."""
-        self.command_list.clear()  # Clear previous commands if any
-        self.command_list.setVisible(True)
+    def send_read_gpio(self):
+        self.send_data('0')  # Send command '0' to read GPIO
 
-        # Adding headers for information (non-clickable items)
-        self.add_non_clickable_item("-- Read GPIO States --")
-        for i in range(1, 7):
-            self.command_list.addItem(f'Read GPIO {i}')
+    def send_set_gpio_high(self, gpio_num):
+        self.send_data(str(gpio_num))  # Send GPIO number as string for High
 
-        self.add_non_clickable_item("-- Set GPIO States --")
-        for i in range(1, 7):
-            self.command_list.addItem(f'Set GPIO {i} Up')
-            self.command_list.addItem(f'Set GPIO {i} Down')
-
-        self.command_list.itemClicked.connect(self.handle_command_click)
-
-    def add_non_clickable_item(self, text):
-        """Add a non-clickable item to the command list."""
-        item = QtWidgets.QListWidgetItem(text)
-        item.setFlags(item.flags() & ~QtCore.Qt.ItemFlag.ItemIsSelectable)  # Remove selection capability
-        item.setFlags(item.flags() & ~QtCore.Qt.ItemFlag.ItemIsEnabled)  # Remove clickability
-        self.command_list.addItem(item)
-
-    def handle_command_click(self, item):
-        """Handle the click event when a command is selected."""
-        command = item.text()
-        if 'Read' in command:
-            gpio_num = command.split(' ')[2]
-            command_to_send = f'READ_GPIO_{gpio_num}'
-        elif 'Set' in command:
-            gpio_num, state = command.split(' ')[2], command.split(' ')[3]
-            command_to_send = f'SET_GPIO_{gpio_num}_{state.upper()}'
-
-        # Insert the command into the input field for user to send later
-        self.input_field.setText(command_to_send)
-
-    def toggle_newline(self, state):
-        """Toggle whether to send commands with \r\n."""
-        self.append_newline = (state == QtCore.Qt.CheckState.Checked)
+    def send_set_gpio_low(self, gpio_num):
+        low_command = str(gpio_num + 4)  # Send GPIO number + 4 for Low (GP2=6, GP3=7, GP4=8, GP5=9)
+        self.send_data(low_command)
 
     def clear_output(self):
         """Clear the output area."""
@@ -138,14 +121,10 @@ class SerialApp(QtWidgets.QWidget):
         self.output_area.append(message)
         self.output_area.setTextColor(QtGui.QColor('black'))  # Reset to default color
 
-    def send_data(self):
+    def send_data(self, data):
         if self.serial_port and self.serial_port.is_open:
-            data = self.input_field.text()
-            if self.append_newline:
-                data += '\r\n'
             self.serial_port.write(data.encode())
             self.output_area.append(f'Sent: {data.strip()}')
-            self.input_field.clear()
 
     def read_data(self):
         if self.serial_port and self.serial_port.is_open:
